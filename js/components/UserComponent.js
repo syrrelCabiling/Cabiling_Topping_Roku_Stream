2d4e760be737fd95dd4041a9ff66242a02cc7190
Courtney Topping <Courtney@Courtneys-MacBook-Pro.local>
2020-04-08 20:44:36 -0400

    splash/userpages updated

--- js/components/UserComponent.js
+++ js/components/UserComponent.js
@@ -5,7 +5,7 @@ export default {
     <div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
         <div class="card rounded" @click="navToUserHome()">
             <div class="card-body text-center">
-                <img :src="'images/user/' + liveuser.avatar + '.jpg'" class="rounded-circle img-fluid">
+                <img :src="'images/user/' + liveuser.avatar + '.png'" class="rounded-circle img-fluid">
                 <p>{{ liveuser.username }}</p>
             </div>
         </div>
